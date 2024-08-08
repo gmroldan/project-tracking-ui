6e61bef56e2b01c595202963582634fc8886527d
Martin Roldan <gmroldanutn@gmail.com>
2024-08-08 15:52:34 +0300

    Add toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TasksBoardComponent } from './tasks-board/tasks-board.component';
-import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSelectModule } from '@angular/material/select';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { FormsModule } from '@angular/forms';
 import { TaskCardComponent } from './task-card/task-card.component';
 import { TaskDetailComponent } from './task-detail/task-detail.component';
@@ -37,6 +38,7 @@ import {
     MatFormFieldModule,
     MatButtonModule,
     MatSelectModule,
+    MatToolbarModule,
     CdkDrag,
     CdkDropList,
     CdkDropListGroup,
